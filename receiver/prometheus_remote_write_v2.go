4df4b89bb28b7448567472a78e175a5b4ccb3d0d
Jiekun <jiekun@victoriametrics.com>
2024-12-17 13:51:14 +0800

    add update decode and unmarshal

--- receiver/prometheus_remote_write_v2.go
+++ receiver/prometheus_remote_write_v2.go
@@ -1,7 +1,6 @@
 package receiver
 
 import (
-	"encoding/json"
 	"github.com/VictoriaMetrics/metrics"
 	"github.com/gin-gonic/gin"
 	"github.com/golang/snappy"
@@ -20,21 +19,22 @@ func NewPrometheusRemoteWriteV2Route(r *gin.Engine) {
 	r.POST("/api/v1/write", func(c *gin.Context) {
 		prometheusRemoteWriteRequestTotal.Inc()
 		b, err := io.ReadAll(c.Request.Body)
+		c.Request.ParseForm()
 		if err != nil {
 			prometheusRemoteWriteReadErrorTotal.Inc()
 			return
 		}
 
 		var body []byte
-		b, err = snappy.Decode(b, body)
+		body, err = snappy.Decode(body, b)
 		if err != nil {
 			log.Printf("snappy.Decode err: %v\n", err)
 			prometheusRemoteWriteDecodeErrorTotal.Inc()
 			return
 		}
 
-		writeRequest := prompb.WriteRequest{}
-		err = json.Unmarshal(b, &writeRequest)
+		writeRequest := &prompb.WriteRequest{}
+		err = writeRequest.Unmarshal(body)
 		if err != nil {
 			log.Printf("json unmarshal write request err: %v\n", err)
 			prometheusRemoteWriteDecodeErrorTotal.Inc()
